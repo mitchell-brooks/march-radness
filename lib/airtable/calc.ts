11b3ebc6a3be4c1a90152f84d10dc9a7a2c8ebd6
Mitchell Brooks <mitchellbrooks@gmail.com>
2022-03-20 01:05:17 -0400

    :bug: Add time zone support
    Add time zone support to correctly dislpay last updated time

--- lib/airtable/calc.ts
+++ lib/airtable/calc.ts
@@ -1,9 +1,9 @@
 import { calcTable } from './connect'
-import { format } from 'date-fns'
-
+//import { format } from 'date-fns'
+import { utcToZonedTime, format } from 'date-fns-tz'
 export async function getUpdateTime() {
   const calc = await calcTable.select({ fields: ['thru'] }).firstPage()
   console.log(calc)
   const time = calc[0].fields.thru
-  return format(new Date(time as string), 'cccc haa')
+  return format(utcToZonedTime(new Date(time as string), 'America/New_York'), 'cccc haa')
 }
